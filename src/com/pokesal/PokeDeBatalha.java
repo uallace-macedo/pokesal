9dc656dd495bac0f7c84946b39f2ecfd9b18f126
Uallace Macedo <uallace.macedo@outlook.com>
2026-09-19 18:29:12 -0300

    fix: atualiza funcao armarParaBatalha

--- src/com/pokesal/PokeDeBatalha.java
+++ src/com/pokesal/PokeDeBatalha.java
@@ -37,18 +37,18 @@ public class PokeDeBatalha {
     	treinador.getMochila().remove(item);
 	}
 
-	public PokeDeBatalha pegarPokeDeBatalha(Treinador treinador){
+	public PokeDeBatalha armarParaBatalha(Treinador treinador) {
 		Pokesal pokesal = treinador.getPokesal();
-		return.treinadorPokesal = new PokeDeBatalha(
+		return new PokeDeBatalha(
 			treinador,
-			pokesal.getNome();
-			pokesal.getHp();
-			pokesal.getDefesa();
-			pokesal.getAtaque();
-			pokesal.getTipoElemento();
-			pokesal.getHabilidade();
-			null;
-
+			pokesal.getNome(),
+			pokesal.getHp(),
+			pokesal.getDefesa(),
+			pokesal.getAtaque(),
+			pokesal.getVelocidade(),
+			pokesal.getTipoElemento(),
+			pokesal.getHabilidades(),
+			null
 		);
 	}
 	
